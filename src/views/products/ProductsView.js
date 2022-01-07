4c5c20c884e8ab2f136fb68ca808e0972652a5bd
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2022-01-07 16:16:14 +0100

    implement refresh token

--- src/views/products/ProductsView.js
+++ src/views/products/ProductsView.js
@@ -27,14 +27,40 @@ class Products extends Component {
     componentDidMount() {
         const keycloak = Keycloak('/keycloak.json');
 
+        keycloak.onTokenExpired = () => {
+            console.log('Token expired', keycloak.token);
+
+            keycloak.updateToken(180).then((refreshed) => {
+                if (refreshed) {                
+                    console.log('Successfully get a new token', keycloak.token);
+
+                    AuthService.instance.init(keycloak);
+                }
+                else 
+                    console.log('Token is still valid');                                
+            })
+            .catch(() => { 
+                console.log('Failed to refresh the token, or the session has expired');
+            });
+        }
+
         keycloak.init({onLoad: 'login-required'}).then(authenticated => {
             AuthService.instance.init(keycloak);
 
-            this.productService.getProducts()
-                .then(data => 
-                    this.setState({ products: data }
-            ));
+            this.getProducts();
         })
+        .catch(function() {
+            console.log('Failed to initialize');
+        });
+    }
+
+    getProducts() {
+        this.productService.getProducts().then(data => 
+            this.setState({ products: data }
+        ))
+        .catch(() => { 
+            console.log('Failed to refresh the products');
+        });
     }
 
     verifiedBodyTemplate(rowData) {
@@ -44,7 +70,6 @@ class Products extends Component {
     logoutClick() {
         AuthService.instance.logout().then(
             response => {
-                // navigate to logon view
                 this.props.history.push("/");
             },
             error => {
@@ -57,21 +82,21 @@ class Products extends Component {
 
     render() {
         return (
-            <div>                
-                <div className="card">
-                    <Button label="Sign Out" icon="pi pi-sign-out" className="w-full" onClick={(e) => this.logoutClick(e)}/>                  
-
-                    <DataTable value={this.state.products} responsiveLayout="scroll">
-                        <Column field="code" header="Code"></Column>
-                        <Column field="description" header="Description"></Column>
-                        <Column field="price" header="Price" dataType="numeric"></Column>
-                        <Column field="active" header="Active" dataType="boolean" body={this.verifiedBodyTemplate}></Column>
-                    </DataTable>
+            <div>
+                <div className="p-d-flex p-jc-between card">
+                    <Button type="button" label="Get Products" icon="pi pi-search" onClick={(event) => this.getProducts(event)}/>                  
+                    <Button type="button" label="Sign Out" icon="pi pi-sign-out" className="p-ml-auto p-button-help" onClick={(event) => this.logoutClick(event)}/>                  
                 </div>
+
+                <DataTable value={this.state.products} responsiveLayout="scroll">
+                    <Column field="code" header="Code"></Column>
+                    <Column field="description" header="Description"></Column>
+                    <Column field="price" header="Price" dataType="numeric"></Column>
+                    <Column field="active" header="Active" dataType="boolean" body={this.verifiedBodyTemplate}></Column>
+                </DataTable>                
             </div>
         )
     }
 }
 
-//export default Products;
 export default withRouter(Products);
